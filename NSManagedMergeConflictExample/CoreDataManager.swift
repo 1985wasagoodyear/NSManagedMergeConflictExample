389176b924b08cf111efca486e72d690f2dfc71d
1985wasagoodyear <1985wasagoodyear>
2019-01-29 22:42:35 -0500

    first commit

---
+++ NSManagedMergeConflictExample/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  NSManagedMergeConflictExample
+//
+//  Created by Kevin Yu on 1/29/19.
+//  Copyright © 2019 Kevin Yu. All rights reserved.
+//
+
+import Foundation
