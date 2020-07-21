5f67d45e0425b5546d014148ac8bb0d6b323b60d
1985wasagoodyear <1985wasagoodyear>
2020-07-21 16:54:09 -0400

    some changes from a while ago.
    TODO: refactor this to make some sense. Write documentation.

--- NSManagedMergeConflictExample/CoreDataManager.swift
+++ NSManagedMergeConflictExample/CoreDataManager.swift
@@ -7,3 +7,125 @@
 //
 
 import Foundation
+import CoreData
+
+/// Adapter for interacting with Merge Policy options
+enum MergePolicyAdapter: Int {
+    case errorMergePolicyType = 0
+    case mergeByPropertyObjectTrumpMergePolicyType
+    case mergeByPropertyStoreTrumpMergePolicyType
+    case overwriteMergePolicyType
+    case rollbackMergePolicyType
+}
+
+final class CoreDataManager {
+    var foo1: Foo!
+    var foo2: Foo!
+    var context1: NSManagedObjectContext
+    var context2: NSManagedObjectContext
+    var controlContext: NSManagedObjectContext  // control context, for main label
+    
+    init() {
+        controlContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context1 = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context2 = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        
+        controlContext.persistentStoreCoordinator = self.persistentContainer.persistentStoreCoordinator
+        context1.persistentStoreCoordinator = self.persistentContainer.persistentStoreCoordinator
+        context2.persistentStoreCoordinator = self.persistentContainer.persistentStoreCoordinator
+    }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "NSManagedMergeConflictExample")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    /// onetime setup, gets initial values for UI
+    func setup() -> (String, String, String) {
+        let request = NSFetchRequest<Foo>(entityName: "Foo")
+        var results = (try? controlContext.fetch(request)) ?? []
+        if results.count == 0 {
+            let newFoo = Foo(context: controlContext)
+            newFoo.val = "Steve"
+            controlContext.insert(newFoo)
+            try? controlContext.save()
+            results = try! controlContext.fetch(request)
+        }
+        let controlFoo = results.first!
+        
+        foo1 = try! context1.fetch(request).first!
+        foo2 = try! context2.fetch(request).first!
+        
+        return (controlFoo.val!, foo1.val!, foo2.val!)
+    }
+
+    /// change merge policy for contexts
+    func setMergePolicy(_ policy: MergePolicyAdapter) {
+        var mergePolicy = NSMergePolicy(merge: .errorMergePolicyType)
+        
+        switch policy {
+        case .errorMergePolicyType:
+            break
+        case .mergeByPropertyObjectTrumpMergePolicyType:
+            mergePolicy = NSMergePolicy(merge: .mergeByPropertyObjectTrumpMergePolicyType)
+        case .mergeByPropertyStoreTrumpMergePolicyType:
+            mergePolicy = NSMergePolicy(merge: .mergeByPropertyStoreTrumpMergePolicyType)
+        case .overwriteMergePolicyType:
+            mergePolicy = NSMergePolicy(merge: .overwriteMergePolicyType)
+        case .rollbackMergePolicyType:
+            mergePolicy = NSMergePolicy(merge: .rollbackMergePolicyType)
+        }
+        
+        context1.mergePolicy = mergePolicy
+        context2.mergePolicy = mergePolicy
+    }
+    
+    func saveToContext1(_ str: String) {
+        foo1.val = str
+        context1.performAndWait {
+            do {
+                try context1.save()
+            }
+            catch {
+                print("Context 1 Save Error:")
+                print(error)
+            }
+        }
+    }
+    
+    func saveToContext2(_ str: String) {
+        foo2.val = str
+        context2.performAndWait {
+            do {
+                try context2.save()
+            }
+            catch {
+                print("Context 2 Save Error:")
+                print(error)
+            }
+        }
+        
+    }
+    
+    /// updates UI, could only get proper update with performAndWait method
+    /// was not responding otherwise, had to reset app to propogate changes
+    func updateControl(_ completion: @escaping (String)->Void) {
+        controlContext.performAndWait {
+            do {
+                let request = NSFetchRequest<Foo>(entityName: "Foo")
+                let controlFoo = try controlContext.fetch(request).first!
+                print("Returning: \(controlFoo.val!)")
+                completion(controlFoo.val!)
+            }
+            catch {
+                print(error)
+                completion("Core Data Error")
+            }
+        }
+    }
+}
--- NSManagedMergeConflictExample/TestViewController.swift
+++ NSManagedMergeConflictExample/TestViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TestViewController.swift
 //  NSManagedMergeConflictExample
 //
 //  Created by Kevin Yu on 1/29/19.
@@ -8,13 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class TestViewController: UIViewController {
 
+    var coreData = CoreDataManager()
+    
+    @IBOutlet var coreDataValueLabel: UILabel!
+    @IBOutlet var mergePolicyButtons: [UIButton]!
+    @IBOutlet var context1TextField: UITextField!
+    @IBOutlet var context2TextField: UITextField!
+    
+    /// currently-selected Merge policy
+    var currMergePolicy: MergePolicyAdapter = MergePolicyAdapter.errorMergePolicyType
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let setupVals = self.coreData.setup()
+        coreDataValueLabel.text = setupVals.0
+        context1TextField.text = setupVals.1
+        context2TextField.text = setupVals.2
     }
-
-
+    
+    /// Helper method to update main info label
+    func updateCoreDataValueLabel() {
+        coreData.updateControl { [weak self] (text) in
+            self?.coreDataValueLabel.text = text
+        }
+    }
+    
+    /// change selected merge policy and update UI
+    @IBAction func mergePolicyButtonAction(_ sender: UIButton) {
+        let option = sender.tag
+        
+        let currButton = self.mergePolicyButtons[currMergePolicy.rawValue]
+        currButton.backgroundColor = .white
+        currButton.setTitleColor(currButton.tintColor, for: .normal)
+        
+        let selectedButton = self.mergePolicyButtons[option]
+        selectedButton.backgroundColor = selectedButton.tintColor
+        selectedButton.setTitleColor(.white, for: .normal)
+        
+        currMergePolicy = MergePolicyAdapter(rawValue: option)!
+        coreData.setMergePolicy(currMergePolicy)
+    }
+    
+    /// save only the first context; update UI
+    @IBAction func context1SaveAction(_ sender: Any) {
+        guard let text = context1TextField.text, text.isEmpty == false else { return }
+        coreData.saveToContext1(text)
+        self.updateCoreDataValueLabel()
+    }
+    
+    /// save only the second context; update UI
+    @IBAction func context2SaveAction(_ sender: Any) {
+        guard let text = context2TextField.text, text.isEmpty == false else { return }
+        coreData.saveToContext2(text)
+        self.updateCoreDataValueLabel()
+    }
+    
+    
+    /// save both contexts, starting with the first one; update UI
+    @IBAction func saveBothAction(_ sender: Any) {
+        guard let text1 = context1TextField.text, text1.isEmpty == false else { return }
+        guard let text2 = context2TextField.text, text2.isEmpty == false else { return }
+        coreData.saveToContext1(text1)
+        coreData.saveToContext2(text2)
+        self.updateCoreDataValueLabel()
+    }
+    
+    
 }
 
